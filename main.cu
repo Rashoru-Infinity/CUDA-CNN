eb2c3e6faeb4c034d54932ccf31fae0ff92055c2
Rashoru-Infinity <65536toaru@gmail.com>
2023-06-08 05:29:13 +0900

    feat: save model and load model

--- main.cu
+++ main.cu
@@ -5,46 +5,85 @@
 
 #include <cuda.h>
 #include <cstdio>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <time.h>
+#include <unistd.h>
 
 static mnist_data *train_set, *test_set;
 static unsigned int train_cnt, test_cnt;
 
 // Define layers of CNN
-static Layer l_input = Layer(0, 0, 28*28);
-static Layer l_c1 = Layer(5*5, 6, 24*24*6);
-static Layer l_s1 = Layer(4*4, 1, 6*6*6);
-static Layer l_f = Layer(6*6*6, 10, 10);
+static Layer *l_input = NULL;
+static Layer *l_c1 = NULL;
+static Layer *l_s1 = NULL;
+static Layer *l_f = NULL;
 
 static void learn();
 static unsigned int classify(double data[28][28]);
 static void test();
 static double forward_pass(double data[28][28]);
 static double back_pass();
+static int write_int(int fd, int src);
+static int write_layer(int fd, Layer *l);
+static int load_layer(int fd, Layer **l);
+static int load_model(const char *file);
+static int write_int(int fd, int src);
+static int write_layer(int fd, Layer *l);
+static int save_model(const char *file);
 
-static inline void loaddata()
+static inline void loaddata(int status)
 {
-	mnist_load("data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte",
-		&train_set, &train_cnt);
+	if (status == -1) {
+	        mnist_load("data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte",
+		        &train_set, &train_cnt);
+	}
 	mnist_load("data/t10k-images.idx3-ubyte", "data/t10k-labels.idx1-ubyte",
 		&test_set, &test_cnt);
 }
 
 int main(int argc, const  char **argv)
 {
+	struct stat buf;
+	int status;
 	srand(time(NULL));
-
 	CUresult err = cuInit(0);
 	if (err != CUDA_SUCCESS) {
 		fprintf(stderr, "CUDA initialisation failed with error code - %d\n", err);
 		return 1;
 	}
-
-	loaddata();
-	learn();
+	if (argc == 1 || (argc >= 2 && (status = stat(argv[1], &buf)) == -1)) {
+		l_input = new Layer(0, 0, 28*28);
+		l_c1 = new Layer(5*5, 6, 24*24*6);
+		l_s1 = new Layer(4*4, 1, 6*6*6);
+		l_f = new Layer(6*6*6, 10, 10);
+		loaddata(-1);
+	        learn();
+	} else {
+		if (load_model(argv[1]) != 0) {
+			goto err;
+		}
+		loaddata(status);
+	}
 	test();
+	if (argc >= 2 && status == -1 && save_model(argv[1])) {
+		return errno;
+	}
 
+	delete l_input;
+	delete l_c1;
+	delete l_s1;
+	delete l_f;
 	return 0;
+err:
+	fprintf(stderr, strerror(errno));
+	delete l_input;
+	delete l_c1;
+	delete l_s1;
+	delete l_f;
+	return errno;
 }
 
 // Forward propagation of a single row in dataset
@@ -58,27 +97,27 @@ static double forward_pass(double data[28][28])
 		}
 	}
 
-	l_input.clear();
-	l_c1.clear();
-	l_s1.clear();
-	l_f.clear();
+	l_input->clear();
+	l_c1->clear();
+	l_s1->clear();
+	l_f->clear();
 
 	clock_t start, end;
 	start = clock();
 
-	l_input.setOutput((float *)input);
+	l_input->setOutput((float *)input);
 	
-	fp_preact_c1<<<64, 64>>>((float (*)[28])l_input.output, (float (*)[24][24])l_c1.preact, (float (*)[5][5])l_c1.weight);
-	fp_bias_c1<<<64, 64>>>((float (*)[24][24])l_c1.preact, l_c1.bias);
-	apply_step_function<<<64, 64>>>(l_c1.preact, l_c1.output, l_c1.O);
+	fp_preact_c1<<<64, 64>>>((float (*)[28])l_input->output, (float (*)[24][24])l_c1->preact, (float (*)[5][5])l_c1->weight);
+	fp_bias_c1<<<64, 64>>>((float (*)[24][24])l_c1->preact, l_c1->bias);
+	apply_step_function<<<64, 64>>>(l_c1->preact, l_c1->output, l_c1->O);
 
-	fp_preact_s1<<<64, 64>>>((float (*)[24][24])l_c1.output, (float (*)[6][6])l_s1.preact, (float (*)[4][4])l_s1.weight);
-	fp_bias_s1<<<64, 64>>>((float (*)[6][6])l_s1.preact, l_s1.bias);
-	apply_step_function<<<64, 64>>>(l_s1.preact, l_s1.output, l_s1.O);
+	fp_preact_s1<<<64, 64>>>((float (*)[24][24])l_c1->output, (float (*)[6][6])l_s1->preact, (float (*)[4][4])l_s1->weight);
+	fp_bias_s1<<<64, 64>>>((float (*)[6][6])l_s1->preact, l_s1->bias);
+	apply_step_function<<<64, 64>>>(l_s1->preact, l_s1->output, l_s1->O);
 
-	fp_preact_f<<<64, 64>>>((float (*)[6][6])l_s1.output, l_f.preact, (float (*)[6][6][6])l_f.weight);
-	fp_bias_f<<<64, 64>>>(l_f.preact, l_f.bias);
-	apply_step_function<<<64, 64>>>(l_f.preact, l_f.output, l_f.O);
+	fp_preact_f<<<64, 64>>>((float (*)[6][6])l_s1->output, l_f->preact, (float (*)[6][6][6])l_f->weight);
+	fp_bias_f<<<64, 64>>>(l_f->preact, l_f->bias);
+	apply_step_function<<<64, 64>>>(l_f->preact, l_f->output, l_f->O);
 	
 	end = clock();
 	return ((double) (end - start)) / CLOCKS_PER_SEC;
@@ -91,23 +130,23 @@ static double back_pass()
 
 	start = clock();
 
-	bp_weight_f<<<64, 64>>>((float (*)[6][6][6])l_f.d_weight, l_f.d_preact, (float (*)[6][6])l_s1.output);
-	bp_bias_f<<<64, 64>>>(l_f.bias, l_f.d_preact);
+	bp_weight_f<<<64, 64>>>((float (*)[6][6][6])l_f->d_weight, l_f->d_preact, (float (*)[6][6])l_s1->output);
+	bp_bias_f<<<64, 64>>>(l_f->bias, l_f->d_preact);
 
-	bp_output_s1<<<64, 64>>>((float (*)[6][6])l_s1.d_output, (float (*)[6][6][6])l_f.weight, l_f.d_preact);
-	bp_preact_s1<<<64, 64>>>((float (*)[6][6])l_s1.d_preact, (float (*)[6][6])l_s1.d_output, (float (*)[6][6])l_s1.preact);
-	bp_weight_s1<<<64, 64>>>((float (*)[4][4])l_s1.d_weight, (float (*)[6][6])l_s1.d_preact, (float (*)[24][24])l_c1.output);
-	bp_bias_s1<<<64, 64>>>(l_s1.bias, (float (*)[6][6])l_s1.d_preact);
+	bp_output_s1<<<64, 64>>>((float (*)[6][6])l_s1->d_output, (float (*)[6][6][6])l_f->weight, l_f->d_preact);
+	bp_preact_s1<<<64, 64>>>((float (*)[6][6])l_s1->d_preact, (float (*)[6][6])l_s1->d_output, (float (*)[6][6])l_s1->preact);
+	bp_weight_s1<<<64, 64>>>((float (*)[4][4])l_s1->d_weight, (float (*)[6][6])l_s1->d_preact, (float (*)[24][24])l_c1->output);
+	bp_bias_s1<<<64, 64>>>(l_s1->bias, (float (*)[6][6])l_s1->d_preact);
 
-	bp_output_c1<<<64, 64>>>((float (*)[24][24])l_c1.d_output, (float (*)[4][4])l_s1.weight, (float (*)[6][6])l_s1.d_preact);
-	bp_preact_c1<<<64, 64>>>((float (*)[24][24])l_c1.d_preact, (float (*)[24][24])l_c1.d_output, (float (*)[24][24])l_c1.preact);
-	bp_weight_c1<<<64, 64>>>((float (*)[5][5])l_c1.d_weight, (float (*)[24][24])l_c1.d_preact, (float (*)[28])l_input.output);
-	bp_bias_c1<<<64, 64>>>(l_c1.bias, (float (*)[24][24])l_c1.d_preact);
+	bp_output_c1<<<64, 64>>>((float (*)[24][24])l_c1->d_output, (float (*)[4][4])l_s1->weight, (float (*)[6][6])l_s1->d_preact);
+	bp_preact_c1<<<64, 64>>>((float (*)[24][24])l_c1->d_preact, (float (*)[24][24])l_c1->d_output, (float (*)[24][24])l_c1->preact);
+	bp_weight_c1<<<64, 64>>>((float (*)[5][5])l_c1->d_weight, (float (*)[24][24])l_c1->d_preact, (float (*)[28])l_input->output);
+	bp_bias_c1<<<64, 64>>>(l_c1->bias, (float (*)[24][24])l_c1->d_preact);
 
 
-	apply_grad<<<64, 64>>>(l_f.weight, l_f.d_weight, l_f.M * l_f.N);
-	apply_grad<<<64, 64>>>(l_s1.weight, l_s1.d_weight, l_s1.M * l_s1.N);
-	apply_grad<<<64, 64>>>(l_c1.weight, l_c1.d_weight, l_c1.M * l_c1.N);
+	apply_grad<<<64, 64>>>(l_f->weight, l_f->d_weight, l_f->M * l_f->N);
+	apply_grad<<<64, 64>>>(l_s1->weight, l_s1->d_weight, l_s1->M * l_s1->N);
+	apply_grad<<<64, 64>>>(l_c1->weight, l_c1->d_weight, l_c1->M * l_c1->N);
 
 	end = clock();
 	return ((double) (end - start)) / CLOCKS_PER_SEC;
@@ -149,13 +188,13 @@ static void learn()
 
 			time_taken += forward_pass(train_set[i].data);
 
-			l_f.bp_clear();
-			l_s1.bp_clear();
-			l_c1.bp_clear();
+			l_f->bp_clear();
+			l_s1->bp_clear();
+			l_c1->bp_clear();
 
 			// Euclid distance of train_set[i]
-			makeError<<<10, 1>>>(l_f.d_preact, l_f.output, train_set[i].label, 10);
-			cublasSnrm2(blas, 10, l_f.d_preact, 1, &tmp_err);
+			makeError<<<10, 1>>>(l_f->d_preact, l_f->output, train_set[i].label, 10);
+			cublasSnrm2(blas, 10, l_f->d_preact, 1, &tmp_err);
 			err += tmp_err;
 
 			time_taken += back_pass();
@@ -184,7 +223,7 @@ static unsigned int classify(double data[28][28])
 
 	unsigned int max = 0;
 
-	cudaMemcpy(res, l_f.output, sizeof(float) * 10, cudaMemcpyDeviceToHost);
+	cudaMemcpy(res, l_f->output, sizeof(float) * 10, cudaMemcpyDeviceToHost);
 
 	for (int i = 1; i < 10; ++i) {
 		if (res[max] < res[i]) {
@@ -208,4 +247,110 @@ static void test()
 
 	fprintf(stdout, "Error Rate: %.2lf%%\n",
 		double(error) / double(test_cnt) * 100.0);
+}
+
+static int load_layer(int fd, Layer **l) {
+	int M, N, O;
+	if (read(fd, &M, sizeof(int)) == -1) {
+		return errno;
+	}
+	if (read(fd, &N, sizeof(int)) == -1) {
+		return errno;
+	}
+	if (read(fd, &O, sizeof(int)) == -1) {
+		return errno;
+	}
+	float bias[N];
+	float weight[N * M];
+	if (read(fd, bias, sizeof(float) * N) == -1) {
+		return errno;
+	}
+	if (read(fd, weight, sizeof(float) * N * M) == -1) {
+		return errno;
+	}
+	*l = new Layer(M, N, O, bias, weight);
+	return 0;
+}
+
+// Load NN model
+static int load_model(const char *file) {
+	int fd;
+	if ((fd = open(file, O_RDONLY)) == -1) {
+		goto err;
+	}
+	if (load_layer(fd, &l_input) == -1) {
+		goto err;
+	}
+	if (load_layer(fd, &l_c1) == -1) {
+		goto err;
+	}
+	if (load_layer(fd, &l_s1) == -1) {
+		goto err;
+	}
+	if (load_layer(fd, &l_f) == -1) {
+		goto err;
+	}
+	close(fd);
+	return 0;
+err:
+	if (fd != -1) {
+		close(fd);
+	}
+	return errno;
+}
+
+static int write_int(int fd, int src) {
+	if (write(fd, &src, sizeof(int)) == -1) {
+		return errno;
+	}
+	return 0;
+}
+
+// Write Layer
+static int write_layer(int fd, Layer *l) {
+	float bias[l->N];
+	float weight[l->N][l->M];
+	//write metadata
+	if (write_int(fd, l->M) == -1) {
+		return errno;
+	}
+	if (write_int(fd, l->N) == -1) {
+		return errno;
+	}
+	if (write_int(fd, l->O) == -1) {
+		return errno;
+	}
+	cudaMemcpy(bias, l->bias, sizeof(float) * l->N, cudaMemcpyDeviceToHost);
+	if (write(fd, bias, sizeof(float) * l->N) == -1) {
+		return errno;
+	}
+	cudaMemcpy(weight, l->weight, sizeof(float) * l->N * l->M, cudaMemcpyDeviceToHost);
+	for (int i = 0;i < l->N;i++) {
+		if (write(fd, weight[i], sizeof(float) * l->M) == -1) {
+			return errno;
+		}
+	}
+	return 0;
+}
+
+// Save NN model
+static int save_model(const char *file) {
+	int fd;
+	if ((fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1) {
+		return errno;
+	}
+	if (write_layer(fd, l_input) == -1) {
+		return errno;
+	}
+	if (write_layer(fd, l_c1) == -1) {
+		return errno;
+	}
+	if (write_layer(fd, l_s1) == -1) {
+		return errno;
+	}
+	if (write_layer(fd, l_f) == -1) {
+		return errno;
+	}
+	close(fd);
+	return 0;
 }
